6f636a77e327b83d0cb3bc65510983b89044aaee
adel <axel.adel761@gmail.com>
2019-11-19 19:43:06 +0100

    adel.js modified

--- adel.js
+++ adel.js
@@ -7,3 +7,13 @@ const b = 1;
 }
 console.log(a);
 console.log(b);
+{
+	const country="Algeria";
+	console.log(`the selected contry is ${country}`);
+}
+{
+	a=9;
+
+	console.log(`${a}+${b}=${a+b}`);
+}
+
